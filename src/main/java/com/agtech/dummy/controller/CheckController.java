6a1aebefc5b32401dfb0f2daa63c40fad404a56c
Amitkumar <chauhan.amitkumar@gmail.com>
2025-03-21 08:10:01 +0800

    []: changes done sysout to check when url is accessed

--- src/main/java/com/agtech/dummy/controller/CheckController.java
+++ src/main/java/com/agtech/dummy/controller/CheckController.java
@@ -8,6 +8,7 @@ public class CheckController {
 
   @GetMapping("/check")
   public String check() {
+    System.out.println("CheckController.check");
     return "Dummy Service Check com.agtech.dummy.controller is working fine";
   }
 }
